8df238382c910abf87cfa5e7437401d5627b442e
Mkrtchyan Mark <isaqwa787@gmail.com>
2024-10-25 03:08:33 +0700

    addind grid

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -11,7 +11,7 @@ const ProductItem = ({product,className,onAdd}) => {
         <div className = {'title'}>{product.title}</div>
         <div className = {'description'}> {product.description}</div>
         <div>
-            <span>Стоимость: <strong>{product.description}</strong></span>
+            <span>Стоимость: <strong>{product.price}</strong></span>
         </div>
         {/* <Button className = {'add-btn'} onClick = {onAddHandler}>
             Добавить в корзину
